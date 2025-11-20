7b340adb586d7889d6a076208e9fc77bc6cf0ec7
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-20 19:16:22 +0500

    Task3

---
+++ Tyuiu.Shahab5.Sprint5.Task3.V24.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.Shahab5.Sprint5.Task3.V24.Lib
+{
+    public class DataService : ISprint5Task3V24
+    {
+        public static string SaveToFileBinaryData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            // Вычисление значения функции (пример формулы)
+            double y = Math.Pow(x, 2) + Math.Sin(x) + 2.5 * x - 1.5;
+            y = Math.Round(y, 3);
+
+            // Запись результата в бинарный файл
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            {
+                writer.Write(y);
+            }
+
+            return path;
+        }
+
+        public string SaveToFileTextData(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.Shahab5.Sprint5.Task3.V24/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Tyuiu.Shahab5.Sprint5.Task3.V24.Lib;
+
+namespace Tyuiu.Shahab5.Sprint5.Task3.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Шахаб А. | АСОиУБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс BinaryWriter. Запись данных в бинарный файл                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Шахаб А. | АСОиУБ-25-1                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 3, результат сохранить  *");
+            Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить    *");
+            Console.WriteLine("* до трёх знаков после запятой.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            Console.WriteLine($"x = {x}");
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = DataService.SaveToFileBinaryData(x);
+
+            // Чтение результата из бинарного файла и вывод на консоль
+            double result;
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                result = reader.ReadDouble();
+            }
+
+            Console.WriteLine($"Результат сохранен в файл: {path}");
+            Console.WriteLine($"Значение функции при x = {x} равно {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
