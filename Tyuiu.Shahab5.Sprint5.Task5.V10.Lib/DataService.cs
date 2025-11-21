6a21099cb4225844e1adc175cc94fd1acfbf671e
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-21 19:29:29 +0500

    Task4

---
+++ Tyuiu.Shahab5.Sprint5.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.Shahab5.Sprint5.Task5.V10.Lib
+{
+    public class DataService : ISprint5Task5V10
+    {
+        public DataService()
+        {
+        }
+
+        public double LoadFromDataFile(string path)
+        {
+            double sum = 0;
+
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+                if (double.TryParse(line, out double number))
+                {
+                    if (number == Math.Floor(number) && number % 2 == 0)
+                    {
+                        sum += number;
+                    }
+                }
+            }
+
+            return Math.Round(sum, 3);
+        }
+    }
+}
+
+
+
---
+++ Tyuiu.Shahab5.Sprint5.Task5.V10.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Tyuiu.Shahab5.Sprint5.Task5.V10.Lib;
+
+namespace Tyuiu.Shahab5.Sprint5.Task5.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V10.txt");
+
+            string[] testData = {
+                "2.5",
+                "4",
+                "6.0",
+                "7.3",
+                "8",
+                "10.2",
+                "12",
+                "3.7"
+            };
+            File.WriteAllLines(path, testData);
+
+            double result = ds.LoadFromDataFile(path);
+            double wait = 30; // 4 + 6 + 8 + 12 = 30
+
+            Assert.AreEqual(wait, result);
+
+            File.Delete(path);
+        }
+    }
+}
